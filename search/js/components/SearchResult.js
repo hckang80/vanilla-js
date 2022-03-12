3bdb4bc202b4ed7095d9344de0a6eda1761408a3
hckang80 <hckang80@gmail.com>
2022-03-12 21:04:54 +0900

    feat: Print list

--- search/js/components/SearchResult.js
+++ search/js/components/SearchResult.js
@@ -1,5 +1,15 @@
 export default function SearchResult ({ $target, list }) {
+  const CLASS_NAME = 'searched-list'
+  const hasWrap = document.querySelector(`.${CLASS_NAME}`)
+  hasWrap && hasWrap.remove()
+
+  const $wrap = document.createElement('ul')
+  $wrap.className = CLASS_NAME
+  $wrap.innerHTML = list.slice(0, 10).map(item => `
+    <li>${item.name}</li>
+  `).join('')
+
   this.render = () => {
-    console.log({ $target, list })
+    $target.insertAdjacentHTML('afterend', $wrap.outerHTML)
   }
 }
